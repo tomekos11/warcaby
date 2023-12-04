d9c20011d391eb110688fb0fefafe1680b01c197
Tomasz Slapinski <tom.slapinski@gmail.com>
2023-12-05 00:37:42 +0100

    gotowe monotwanie komponentu tylko ogarnac zmienne nazwy po zbundlowaniu

--- My_app/frontend/js/app.js
+++ My_app/frontend/js/app.js
@@ -4,4 +4,4 @@ import App from './components/App.vue'
 const app = createApp(App);
 
 
-app.mount('app');
+app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     outDir: 'My_app/frontend/static', // Specify your desired output directory
-    publicDir: 'My_app/frontend/static',
     assetsDir: '',
     rollupOptions: {
       input: 'My_app/frontend/js/app.js', // specify your entry file here
