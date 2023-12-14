fb8973f44e7e7426c1274e815ee3b988770a8a44
Mikołaj Salamak <mikolaj.salamak@stormcode.pl>
2023-12-14 22:35:54 +0100

    Masz :*

--- My_app/cv_optimised/test.py
+++ My_app/cv_optimised/test.py
@@ -80,15 +80,15 @@ class ImageProcess:
         for i in range(8):
             for j in range(8):
                 if board[i][j] == 1:
-                    board[i][j] = 'W'
+                    board[i][j] = 1
                 elif board[i][j] == -1:
-                    board[i][j] = 'B'
+                    board[i][j] = -1
                 elif board[i][j] == 2:
-                    board[i][j] = 'WK'
+                    board[i][j] = 2
                 elif board[i][j] == -2:
-                    board[i][j] = 'BK'
+                    board[i][j] = -2
                 else:
-                    board[i][j] = '-'
+                    board[i][j] = 0
 
         return np.rot90(board, k=2)
     
@@ -128,78 +128,74 @@ class ImageProcess:
         for i in circles[0]:
             # print(w, h, i)
             # print(w + 0.1 * w > 2 * i[2], h + 0.1 * h  > 2 * i[2], w * 0.35 < i[2], h * 0.35 < i[2])
-            if (w + 0.1 * w > 2 * i[2] or h + 0.1 * h > 2 * i[2]) and (w * 0.35 < i[2] or h * 0.35 < i[2]):
+            if (w + 0.1 * w > 2 * i[2] or h + 0.1 * h > 2 * i[2]) and (w * 0.3 < i[2] or h * 0.3 < i[2]):
                 return True, i
         # print("DIM: ", x, y, w, h)
         # print(circles[0]) 
         return False, []
-        
+
     def searchForPawn(self, img, pos):
         # Przygotowanie obrazu do oceny 
+        img_color = copy.deepcopy(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img_gray = copy.deepcopy(img)
         img = cv2.medianBlur(img, 5)
-        img_copy = copy.deepcopy(img) # to ewentualnie moze byc przed Blurem, chociaż tak wydaje sie ok.
+        img_copy = copy.deepcopy(img)  # to ewentualnie moze byc przed Blurem, chociaż tak wydaje sie ok.
         # Binaryzacja
-        x, dst = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
+        x, dst = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
         # Poprzedni sposob, niby dziala, ale nie dziala
         """#edges = cv2.GaussianBlur(img3, (3, 3), 0)
         #edges = cv2.Canny(edges, self.param1, self.param2)        
         #circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 20, param1=70, param2=20, minRadius=10, maxRadius=0)"""
-
         # Transformacja Hougha do znalezienia okręgów na obrazie, todo dodanie ustawiania
-        circles = cv2.HoughCircles(copy.deepcopy(img), cv2.HOUGH_GRADIENT, 1, 20, param1=150 , param2=24 , minRadius=0, maxRadius=100)
+        circles = cv2.HoughCircles(copy.deepcopy(img), cv2.HOUGH_GRADIENT, 1, 20, param1=150, param2=24, minRadius=0,
+                                   maxRadius=100)
         # Nie ma okregow, to 0
         if circles is not None:
             # Na inty
             circles = np.uint16(np.around(circles))
-            #Rysowanie do testów
-            """for i in circles[0, :]:
+            # Rysowanie do testów
+            for i in circles[0, :]:
                 cv2.circle(img, (i[0], i[1]), i[2], (255, 255, 255), 2)
-            board.showActualImage(img, 'circles')"""
+            # board.showActualImage(img, 'circles')
             # Sprawdzenie okregow, wybranie najbardziej prawdopodobnego
             isGood, circle = self.checkCircleIsCorrect(img_gray, circles)
             if not isGood:
                 return 0
         else:
             return 0
-        #Rysowanie jak poszło wyszukiwanie okregow
-        # for i in circles[0, :]:
-        #     cv2.circle(img, (i[0], i[1]), i[2], (255, 255, 255), 2)
+        # Rysowanie jak poszło wyszukiwanie okregow
+
         radius = circle[2]
         # Ocena, czy na obrazie znajduje sie drugi okrag - damka
-        isKing = self.checkIsKing(img_copy, radius, circle) 
+        isKing = self.checkIsKing(img_copy, radius, circle)
         # Obliczenie sredniej pikseli wokol srodka okregu
         d = int(circle[2] / 1.6)
         x = circle[0]
         y = circle[1]
-
-        suma = 0
-        divi = 0
-        for row in dst[y - d:y + d, x - d:x + d]:
-            for px in row:            
-                suma += px
-                divi += 1
-        try:
-            suma /= divi
-        except ZeroDivisionError:
-            pass 
-        
-        thr = 150  # todo ustawienie progu, bo tutaj mocno kolor decyduje
-        # Wypisanie wartosci oraz rysowanie okregu na obrazie
-        #print(suma, thr)
-        """if suma > thr:
-            cv2.circle(img, (el[0], el[1]), el[2], (255, 0, 0), 2)
-        else:
-            cv2.circle(img, (el[0], el[1]), el[2], (255, 255, 255), 2)"""
-        # Ocena na podstawie sredniej jakiego koloru jest pionek
-        if suma > thr:
+        suma = [0, 0, 0]
+        x_p = y - d if y - d > 0 else 0
+        y_p = x - d if x - d > 0 else 0
+        for row in img_color[x_p:y + d, y_p:x + d]:
+            for px in row:
+                suma[0] += px[0]
+                suma[1] += px[1]
+                suma[2] += px[2]
+                # bgr, b > g
+        # board.showActualImage(img_color, 'circles')
+        # print(suma)
+        if suma[0] > suma[1]:
             return 2 if isKing else 1
         else:
-            return -2 if isKing else -1        
+            return -2 if isKing else -1
+        # """if suma > thr:
+        #     cv2.circle(img, (el[0], el[1]), el[2], (255, 0, 0), 2)
+        # else:
+        #     cv2.circle(img, (el[0], el[1]), el[2], (255, 255, 255), 2)"""
 
 class Board:
     def __init__(self, image):
+        print(image)
         self.image = cv2.imread(image)
         self.gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.board = self.getOnlyBoard(self.image)
@@ -313,7 +309,7 @@ class Board:
 
 if __name__ == "__main__":
     script_directory = os.path.dirname(os.path.abspath(__file__))
-    image_path = os.path.join(script_directory, 'plansza.jpg')
+    image_path = os.path.join(script_directory, './plansza.jpg')
     board = Board(image_path)# Wycięcie samej planszy z obrazu
     if board.board is not None:
         proc = ImageProcess(board.board)
